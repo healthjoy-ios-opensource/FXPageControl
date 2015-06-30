b9e70c6ae92eb10d8e5f897f83b11896ea3f49a6
Mark Prutskiy <m.prutskiy@healthjoy.com>
2015-06-30 12:20:18 +0300

    Created dynamic framework

---
+++ FXPageControl-iOS/FXPageControl-dynamic/FXPageControl-dynamic.h
@@ -0,0 +1,19 @@
+//
+//  FXPageControl-dynamic.h
+//  FXPageControl-dynamic
+//
+//  Created by Mark Mark on 6/30/15.
+//  Copyright (c) 2015 Mark Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FXPageControl-dynamic.
+FOUNDATION_EXPORT double FXPageControl_dynamicVersionNumber;
+
+//! Project version string for FXPageControl-dynamic.
+FOUNDATION_EXPORT const unsigned char FXPageControl_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXPageControl_dynamic/PublicHeader.h>
+
+
