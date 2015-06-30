149ae201bcee37c0bcadf47fade9135a746dc61d
Mark Prutskiy <m.prutskiy@healthjoy.com>
2015-06-30 12:27:51 +0300

    Created static framework

---
+++ FXPageControl-iOS/FXPageControl-static/FXPageControl-static.h
@@ -0,0 +1,19 @@
+//
+//  FXPageControl-static.h
+//  FXPageControl-static
+//
+//  Created by Mark Mark on 6/30/15.
+//  Copyright (c) 2015 Mark Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FXPageControl-static.
+FOUNDATION_EXPORT double FXPageControl_staticVersionNumber;
+
+//! Project version string for FXPageControl-static.
+FOUNDATION_EXPORT const unsigned char FXPageControl_staticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXPageControl_static/PublicHeader.h>
+
+
